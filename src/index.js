bf762b031a710cea81c48319f21705b489da6d19
Maksym Lukianenko <maks.lukyan@gmail.com>
2023-01-23 02:41:23 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryRef = document.querySelector('.gallery');
 const searchForm = document.querySelector('#search-form');
+const loadMoreBtn = document.querySelector('.load-more');
 
 let gallery = new SimpleLightbox('.gallery a');
 let query = '';
@@ -14,13 +15,14 @@ let page = 1;
 const perPage = 40;
 
 searchForm.addEventListener('submit', onFormSearch);
+loadMoreBtn.addEventListener('click', onLoadMoreBtn);
 
 function onFormSearch(evt) {
   evt.preventDefault();
   page = 1;
   const query = evt.target.searchQuery.value.trim().toLowerCase();
   galleryRef.innerHTML = '';
-  console.log(query);
+  loadMoreBtn.classList.add('is-hidden');
 
   if (!query) {
     Notify.failure(
@@ -40,11 +42,42 @@ function onFormSearch(evt) {
         gallery.refresh();
         Notify.success(`Hooray! We found ${data.totalHits} images.`);
       }
+      if (data.totalHits > perPage) {
+        loadMoreBtn.classList.remove('is-hidden');
+      }
     })
     .catch(error => console.log(error))
     .finally(() => {
       searchForm.reset();
     });
+}
+
+function onLoadMoreBtn() {
+  page += 1;
+  fetchImage(query, page, perPage)
+    .then(({ data }) => {
+      renderGallery(data.hits);
+      gallery.refresh();
+      smoothScroll();
+      const totalPages = Math.ceil(data.totalHits / perPage);
+      if (page > totalPages) {
+        loadMoreBtn.classList.add('is-hidden');
+        Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+    })
+    .catch(error => console.log(error));
+}
+function smoothScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
 }
 
 // function addListMarkup(markup = '') {
